d5579034397903e5afdcfcd45aa175b39765a67d
Gareth Sears <garethsears@gmail.com>
2020-02-28 23:13:04 +0000

    add Hand

---
+++ src/main/java/tech/hootlab/Hand.java
@@ -0,0 +1,46 @@
+package tech.hootlab;
+
+/*
+ * Hand.java Gareth Sears - 2493194S
+ */
+
+/**
+ * A class which represents a players hand.
+ */
+public class Hand extends Deck implements Comparable<Hand> {
+
+    int value = 0;
+
+    /**
+     * Adds a card to the deck, but also increments the value of the hand with it.
+     */
+    @Override
+    public void add(Card card) {
+        Ranks rank = card.getRank();
+
+        if (rank == Ranks.ACE) {
+            if (value < 11) {
+                value += 11;
+            } else {
+                value += 1;
+            }
+        } else {
+            value += rank.getValue();
+        }
+
+        super.add(card);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * Allows easy comparisons with other hands based on value.
+     */
+    @Override
+    public int compareTo(Hand o) {
+        return value - o.getValue();
+    }
+
+}
