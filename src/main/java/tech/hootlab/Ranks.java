c3a761f4c2fe985504e35154becc9601199a87ef
Gareth Sears <garethsears@gmail.com>
2020-02-28 23:13:31 +0000

    remove whitespace

--- src/main/java/tech/hootlab/Ranks.java
+++ src/main/java/tech/hootlab/Ranks.java
@@ -12,14 +12,14 @@ public enum Ranks {
     TWO(2),
     THREE(3),
     FOUR(4),
-    FIVE(5), 
-    SIX(6), 
-    SEVEN(7), 
-    EIGHT(8), 
-    NINE(9), 
-    TEN(10), 
-    JACK(10), 
-    QUEEN(10), 
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
     KING(10);
 
     private int val;
