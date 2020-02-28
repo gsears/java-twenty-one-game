117ba21e3888117ef24967cb2f20c31c7b7ae0e1
Gareth Sears <garethsears@gmail.com>
2020-02-28 22:43:47 +0000

    Assign values to ranks

--- src/main/java/tech/hootlab/Ranks.java
+++ src/main/java/tech/hootlab/Ranks.java
@@ -8,5 +8,33 @@ package tech.hootlab;
  * An enum representing the different possible card ranks.
  */
 public enum Ranks {
-    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    ACE(11),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5), 
+    SIX(6), 
+    SEVEN(7), 
+    EIGHT(8), 
+    NINE(9), 
+    TEN(10), 
+    JACK(10), 
+    QUEEN(10), 
+    KING(10);
+
+    private int val;
+
+    private Ranks(int val) {
+        this.val = val;
+    }
+
+    /**
+     * Returns the card value.
+     * Note that ACE defaults to 11 and the 'royals' to 10.
+     * @return The card value
+     */
+    public int getValue() {
+        return val;
+    }
+
 }
