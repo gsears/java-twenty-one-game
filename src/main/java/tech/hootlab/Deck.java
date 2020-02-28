5afd8c728a3a9d25bece7f982cdca78e13ea105b
Gareth Sears <garethsears@gmail.com>
2020-02-28 23:12:53 +0000

    refactor deck to allow for inheritance by hand

--- src/main/java/tech/hootlab/Deck.java
+++ src/main/java/tech/hootlab/Deck.java
@@ -9,27 +9,27 @@ import java.util.List;
  */
 
 /**
- * A class representing a deck of cards.
+ * A class representing a collection of cards. Note: this doesn't necessarily mean the full '52'
+ * cards, rather a 'collection' of cards of any size.
  */
 public class Deck {
-    List<Card> cardList = new LinkedList<>();
+    // Define as linked list so we can use list (shuffle) and queue (poll) functionality.
+    LinkedList<Card> cardList = new LinkedList<>();
 
     /**
-     * Create a new, unshuffled deck...fresh from the packet.
+     * A static utility method for getting your bog-standard deck of cards from the ranks and suits
+     * provided.
+     *
+     * @return
      */
-    public Deck() {
-        initialise();
-    }
-
-    /**
-     * Populate the deck with cards from ranks and suits.
-     */
-    private void initialise() {
+    public static Deck getStandardDeck() {
+        Deck deck = new Deck();
         for (Suits suit : Suits.values()) {
             for (Ranks rank : Ranks.values()) {
-                cardList.add(new Card(suit, rank));
+                deck.add(new Card(suit, rank));
             }
         }
+        return deck;
     }
 
     public List<Card> getCardList() {
@@ -41,5 +41,32 @@ public class Deck {
      */
     public void shuffle() {
         Collections.shuffle(cardList);
+    }
+
+    /**
+     * Takes the top card off the deck and returns it.
+     *
+     * @return
+     */
+    public Card deal() {
+        return cardList.poll();
+    }
+
+    /**
+     * Add a single card to the bottom of this deck.
+     *
+     * @param card The card to add.
+     */
+    public void add(Card card) {
+        cardList.addLast(card);
+    }
+
+    /**
+     * Add a deck to the bottom of this deck.
+     *
+     * @param deck The deck to add.
+     */
+    public void add(Deck deck) {
+        cardList.addAll(deck.getCardList());
     }
 }
