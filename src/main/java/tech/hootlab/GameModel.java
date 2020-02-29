4958049098142107da8f29353c3694f4d1519055
Gareth Sears <garethsears@gmail.com>
2020-02-29 21:18:49 +0000

    update GameModel to remove round logic

--- src/main/java/tech/hootlab/GameModel.java
+++ src/main/java/tech/hootlab/GameModel.java
@@ -16,25 +16,12 @@ public class GameModel {
     public final int INITIAL_TOKENS = 200;
     public final int STAKE = 20;
 
-    boolean isGameActive = false;
-    // Game deck
-    Deck deck;
     // Players
     List<Player> playerList = new LinkedList<>();
     // Round
     Round round;
     // Dealer
     Player dealer;
-
-
-    public GameModel() {
-        reset();
-    }
-
-    private void reset() {
-        deck = Deck.getStandardDeck();
-        deck.shuffle();
-    }
 
     // Players added and removed from the game should be added on next round.
     public void addPlayer(Player player) {
