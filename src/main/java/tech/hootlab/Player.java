33fa3293e573ce28c2f182e23fe138d250750e44
Gareth Sears <garethsears@gmail.com>
2020-02-29 14:11:52 +0000

    update Player to include transferTokens

--- src/main/java/tech/hootlab/Player.java
+++ src/main/java/tech/hootlab/Player.java
@@ -1,16 +1,37 @@
 package tech.hootlab;
 
 public class Player {
-    String name;
-    int bank; // The money / chips / whatever...
-    Hand hand = new Hand();
+    private String name;
+    private int tokens; // The money / chips / whatever...
+    private Hand hand = new Hand();
 
-    public Player(String name, int initialBank) {
+    public Player(String name, int initialTokens) {
         this.name = name;
-        this.bank = initialBank;
+        this.tokens = initialTokens;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public Hand getHand() {
         return hand;
     }
+
+    public int getTokens() {
+        return tokens;
+    }
+
+    public void transferTokens(Player target, int numTokens) {
+        if (numTokens < tokens) {
+            // Transfers as many tokens as they can, rinse them out!
+            target.tokens += tokens;
+            tokens = 0;
+        } else {
+            target.tokens += numTokens;
+            tokens -= numTokens;
+        }
+    }
+
+
 }
